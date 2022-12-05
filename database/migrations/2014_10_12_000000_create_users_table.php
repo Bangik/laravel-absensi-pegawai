16fe8d0643185d28a23ff2c410e806f83ec0ce76
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2022-12-05 18:43:51 +0700

    Create mgt, model, seed of role and present

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Database\Seeders\UserSeeder;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,13 +16,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->string('name');
+            $table->string('nrp')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('avatar')->nullable();
             $table->timestamps();
         });
+
+        $seed = new UserSeeder();
+        $seed->run();
     }
 
     /**
