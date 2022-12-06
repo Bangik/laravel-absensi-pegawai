340e7ee4399290d072ab0ef0bb22f4e017d99e38
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2022-12-06 19:01:27 +0700

    update home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Present;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,23 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $present = Present::whereUserId(auth()->user()->id)->whereDates(date('Y-m-d'))->first();
+        $url = 'https://kalenderindonesia.com/api/YZ35u6a7sFWN/libur/masehi/'.date('Y/m');
+        $kalender = file_get_contents($url);
+        $kalender = json_decode($kalender, true);
+        $libur = false;
+        $holiday = null;
+        if ($kalender['data'] != false) {
+            if ($kalender['data']['holiday']['data']) {
+                foreach ($kalender['data']['holiday']['data'] as $key => $value) {
+                    if ($value['date'] == date('Y-m-d')) {
+                        $holiday = $value['name'];
+                        $libur = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return view('home', compact('present','libur','holiday'));
     }
 }
